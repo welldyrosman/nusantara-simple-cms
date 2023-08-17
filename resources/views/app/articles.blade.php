15b1290a4a26fdeead0b9cd74d4a94a72b55f560
welldyrosman <welldyrosman@gmail.com>
2023-08-17 13:17:14 +0700

    update

--- resources/views/app/articles.blade.php
+++ resources/views/app/articles.blade.php
@@ -1,14 +1,35 @@
-@extends('layouts.app')ß
+@extends('layouts.app')
 @section('content')
     <section class="section w-100">
         <div class="container">
             <div class="row">
                 <div class="col-md-9">
-                    <div class="mt-5">
-                        <div class="row">
-                            <div class="col-8"></div>
-                            <div class="col-4"></div>
+                    <div class="row mt-5">
+                        <div class="col-8">
+                            <div class="imagecrop">
+                                <img src="{{ $articles[0]->image ? asset($articles[0]->image) : 'https://picsum.photos/600/300' }}" alt="Gambar Kiri" class="img" />
+
+                                <div class="gradient-overlay"></div>
+                                <div class="overlay-text"><a href="{{ $articles[2]->link }}">{{$articles[0]->title}}</a></div>
+                            </div>
                         </div>
+                        <div class="col-4">
+                            <div class="imagecrop kecil mb-3">
+                                <img src="{{ $articles[1]->image ? asset($articles[1]->image) : 'https://picsum.photos/600/300' }}" alt="Gambar Kiri" class="img" />
+
+                                <div class="gradient-overlay"></div>
+                                <div class="overlay-text">  <a href="{{ $articles[2]->link }}">{{$articles[1]->title}}</a></div>
+                            </div>
+                            <div class="imagecrop kecil">
+                                <img src="{{ $articles[2]->image ? asset($articles[2]->image) : 'https://picsum.photos/600/300' }}" alt="Gambar Kiri" class="img" />
+
+                                <div class="gradient-overlay"></div>
+                                <div class="overlay-text"><a href="{{ $articles[2]->link }}">{{$articles[2]->title}}</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-5">
+                        
                         @include('partials.app.articlelist')
                     </div>
                 </div>
