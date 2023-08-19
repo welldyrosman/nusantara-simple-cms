2ea53c33a1e7edbbcd9392bf1a6b3f722b9a0e35
welldyrosman <welldyrosman@gmail.com>
2023-08-19 14:58:39 +0700

    fixs tags

--- resources/views/partials/admin/form/tagtify.blade.php
+++ resources/views/partials/admin/form/tagtify.blade.php
@@ -1,9 +1,10 @@
+
 <div class="field">
     <label class="label">{{ __('admin.fields.' . $resource . '.' . $attribute) }}</label>
     <div class="control">
-        @php 
-            $value = $article->tags ? $article->tags->pluck('tag')->implode(',') : '';
+        @php
+            $value = $article && $article->tags ? $article->tags->pluck('tag')->implode(',') : '';
         @endphp
-        <input name="{{ $attribute }}" value="{{ empty($value) ? ($default ?? '') : $value }}" data-role="tagsinput" class=" form-control input is-large {{ $class ?? '' }}" autofocus>
+        <input name="{{ $attribute }}" value="{{ empty($value) ? ($default ?? '') : $value }}" data-role="tagsinput" class="form-control input is-large {{ $class ?? '' }}" autofocus>
     </div>
 </div>
